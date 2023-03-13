36e70187743f759a62586696574811414c6beedc
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2023-03-13 07:35:43 +0300

    Doing things about the parser.ts library...

--- src/utils.ts
+++ src/utils.ts
@@ -1,38 +1,71 @@
 // TODO: whenever functions are concerned, pray do add capability to add as many arguments as he the self wants...
-// TODO: Create some of the wanted elementary functions that self would consider useful in building a parser (that is, to maximize the optimization of the routine repetition of various naturally ocurring parsing procedures)...
-// TODO: let the things that are `start`, `stop` and `separator` be more general: don't have to be `string`, anything else...
 
-// TODO: Creepy arguments names... Do something about them...
-// TODO: the "noStartSym" argument feels kind of obsolete... Should self not do something about it?.. Instead using a flag for this sort of thing? At the same time... This may be more useful for the UtilFunctions... Though, it can work both ways...
-export function delimited<StringType = string>(
-	start: StringType[],
-	stop: StringType[],
-	separator: StringType[],
-	noStartSym: StringType,
-	noStopSym: StringType,
-	parser: Function,
-	shouldSkipStop: boolean | Function = true,
-	noticeFunc: Function,
-	nextFunc: Function,
-	skip: (toSkip: StringType, noticeFunction?: Function) => any,
-	endFunc: (...a: any) => boolean,
-	startCallback: Function = () => {
+// TODO: Create some of the wanted elementary functions that self would consider useful in building a parser (that is, to maximize the optimization of the routine repetition of various naturally ocurring parsing procedures)...
+// * Currently, the `utils` can:
+// * 1. delimit arbitrary sequences...
+// * 2. read arbitrary functions provided the functions tables themselves...
+// * 3. read arbitrary sequences of tables provided sequences of functions tables...
+// * 4. recursively read a tree-object of arbitrary depth or shape;
+// * 5. recursively write a tree-object of arbitrary depth or shape;
+// * 6. configurably skip past multiple instances of a certain object;
+// * 7. configurably read a sequence of objects and then configurably "append" them together (depends on user's definition...)
+// IN WORK: 8. configure a united system of all these functions...
+// * 9. Have a defined type of a function table
+
+// TODO: continue on with the generalizing of the things...
+
+// ? Rename arguments?
+export function delimited<StringType = string>({
+	starts,
+	stops,
+	separators,
+	parser,
+	shouldSkipStop = true,
+	noStartSym,
+	noStopSym,
+	noticeFunc,
+	nextFunc,
+	skip,
+	endFunc,
+	outputFunction,
+	startCallback = () => {
 		return undefined
 	},
-	endCallback: Function = () => {
+	endCallback = () => {
 		return undefined
 	},
+	skipMultipleSeps = true,
+}: {
+	starts: StringType[]
+	stops: StringType[]
+	separators: StringType[]
+	parser: Function
+	shouldSkipStop: boolean | Function
+	noStartSym: StringType
+	noStopSym: StringType
+	noticeFunc: Function
+	nextFunc: Function
+	skip: (toSkip: StringType, noticeFunction?: Function) => any
+	endFunc: (...a: any) => boolean
 	outputFunction: (beginVal: any, endVal: any, delimitedRes: any[]) => any
-) {
+	startCallback?: Function
+	endCallback?: Function
+	skipMultipleSeps?: boolean
+}) {
 	// ? generalize?
 	function skipSeparators(separator: StringType) {
 		skip(separator, noticeFunc)
-		skipMultiple<StringType>(separator, nextFunc, noticeFunc)
+		if (skipMultipleSeps)
+			skipMultiple<StringType>({
+				char: separator,
+				nextFunction: nextFunc,
+				spotFunction: noticeFunc,
+			})
 	}
 
 	const tocall_end = () => {
 		let i = 1
-		for (const s of stop) {
+		for (const s of stops) {
 			if (noticeFunc(s)) return i
 			i++
 		}
@@ -42,24 +75,24 @@ export function delimited<StringType = string>(
 	const delimitedArr: any[] = []
 	let first = true
 
-	const stopOnSeparator = stop.includes(noStopSym)
+	const stopOnSeparator = stops.includes(noStopSym)
 	let startVal = undefined
 	let endVal: any
 
 	// allowing for multiple starts (here, the starts are skipped "-or"-way, not "-and"-way; i.e. if more than one are found, then only the first one is read...)
 	// ? why, by the way? Should he the body not make it a flag?
-	if (!start.includes(noStartSym))
-		if (start instanceof Array) {
+	if (!starts.includes(noStartSym))
+		if (starts instanceof Array) {
 			let i = 0
 
-			for (const _start of start) {
+			for (const _start of starts) {
 				if (noticeFunc(_start)) {
 					startVal = startCallback()
 					nextFunc()
 					break
 				}
 
-				if (i === start.length - 1) {
+				if (i === starts.length - 1) {
 					startVal = startCallback()
 					skip(_start, noticeFunc)
 				}
@@ -69,7 +102,12 @@ export function delimited<StringType = string>(
 
 	// ? this "skip the separators before the actual beginning" should be turnable on/off by a flag too...
 	// TODO: do along with the other questions...
-	for (const a of separator) skipMultiple<StringType>(a, nextFunc, noticeFunc)
+	for (const a of separators)
+		skipMultiple<StringType>({
+			char: a,
+			nextFunction: nextFunc,
+			spotFunction: noticeFunc,
+		})
 
 	while (!endFunc()) {
 		if (!stopOnSeparator && tocall_end()) {
@@ -82,26 +120,26 @@ export function delimited<StringType = string>(
 			if (!stopOnSeparator) {
 				let j = 0
 
-				for (const sep of separator) {
+				for (const sep of separators) {
 					if (noticeFunc(sep)) {
 						skipSeparators(sep)
 						break
 					}
 
-					if (j === separator.length - 1) skipSeparators(sep)
+					if (j === separators.length - 1) skipSeparators(sep)
 					j++
 				}
-			} else if (separator instanceof Array) {
+			} else if (separators instanceof Array) {
 				let leave = false
 
-				for (const s of separator)
+				for (const s of separators)
 					if (noticeFunc(s)) {
 						leave = true
 						break
 					}
 
 				if (!leave) break
-			} else if (!noticeFunc(separator)) break
+			} else if (!noticeFunc(separators)) break
 		}
 
 		if (!stopOnSeparator && tocall_end()) {
@@ -116,59 +154,135 @@ export function delimited<StringType = string>(
 
 	if (typeof shouldSkipStop === "function") shouldSkipStop = shouldSkipStop()
 	if (!stopOnSeparator && tocallendres && shouldSkipStop)
-		skip(stop[(tocallendres as number) - 1], noticeFunc)
+		skip(stops[(tocallendres as number) - 1], noticeFunc)
 
 	return outputFunction(startVal, endVal, delimitedArr)
 }
 
-function skipMultiple<StringType>(
-	char: StringType,
-	nextFunction: Function,
+function skipMultiple<StringType = string>({
+	char,
+	nextFunction,
+	spotFunction,
+}: {
+	char: StringType
+	nextFunction: Function
 	spotFunction: Function
-) {
+}) {
 	while (spotFunction(char)) nextFunction()
 }
 
-export function readWhilst<StringType = string>(
-	predicate: Function,
-	currentFunction: Function,
-	nextFunction: Function,
-	appendFunction: Function,
-	endFunc: () => boolean,
-	emptyValue: StringType,
-	finale?: Function,
-	skipFirst = false
-) {
+export function readWhilst<StringType = string>({
+	predicate,
+	currentFunction,
+	nextFunction,
+	appendFunction,
+	beforeEvery = () => undefined,
+	afterEvery = () => undefined,
+	endFunc,
+	emptyValue,
+	beginning = () => undefined,
+	finale = () => undefined,
+	skipFirst = false,
+}: {
+	predicate: Function
+	currentFunction: Function
+	nextFunction: Function
+	appendFunction: Function
+	beforeEvery: Function
+	afterEvery: Function
+	endFunc: () => boolean
+	emptyValue: StringType
+	beginning: Function
+	finale: Function | undefined
+	skipFirst?: boolean
+}): StringType {
 	let res: StringType = skipFirst ? emptyValue : currentFunction()
-	while (!endFunc() && predicate(currentFunction()))
-		res = appendFunction(res, nextFunction)
-	if (finale) finale()
+	beginning()
+	while (!endFunc() && predicate(currentFunction())) {
+		beforeEvery()
+		res = appendFunction(res, nextFunction())
+		afterEvery()
+	}
+	finale()
 	return res
 }
 
 export const readWhile = readWhilst
 
 // * useful with the pre-set tokentypes inside the UtilFunctions (simulating the proper templates)...
-export function read(
-	tokentypes: { [a: string]: Function },
-	tokentype: string,
-	...args: any[]
-): any {
-	return tokentypes[tokentype](...args)
+export function read({
+	typestable,
+	type,
+	args = [],
+}: {
+	typestable: FunctionTable
+	type: Key
+	args?: any[]
+}): any {
+	return typestable[type](...args)
+}
+
+// * generalization of read for multiple types;
+export function readSequence({
+	typestable,
+	types,
+	args = Object.keys(typestable).map(() => []),
+}: {
+	typestable: FunctionTable[]
+	types: Key[]
+	args?: any[][]
+}): any {
+	let currRes: any = undefined
+
+	for (let i = 0; i < types.length; i++) {
+		currRes = typestable[types[i]](currRes, args[i])
+		if (!currRes) break
+	}
+
+	return currRes
 }
 
 // * May be very useful in parsing of nested things. Used it once for an algorithm to traverse an arbitrary binary sequence...
-export function recursiveIndexation<InType = object, OutType = any>(
-	object: InType,
-	fields: (string | symbol | number)[]
-): OutType {
+export function recursiveIndexation<InType = object, OutType = any>({
+	object,
+	fields,
+}: {
+	object: InType
+	fields: Key[]
+}): OutType {
 	let res: any = object
 	for (const f of fields) res = res[f]
 	return res as OutType
 }
 
+export function recursiveSetting<InType = object, OutType = any>({
+	object,
+	fields,
+	value,
+}: {
+	object: InType
+	fields: Key[]
+	value: any
+}): OutType {
+	let result: any = object
+
+	result = recursiveIndexation({
+		object: result,
+		fields: fields.slice(0, fields.length - 1),
+	})
+
+	result[fields[fields.length - 1]] = value
+	return result as OutType
+}
+
 // * syntax sugar for while () {}
-export function whileDo(endfunction: () => boolean, repeat: Function) {
+export function whileDo({
+	endfunction,
+	repeat,
+}: {
+	endfunction: () => boolean
+	repeat: Function
+}): void {
 	while (endfunction()) repeat()
 }
 
@@ -178,14 +292,27 @@ export function whileDo(endfunction: () => boolean, repeat: Function) {
 // TODO: after having created all the wanted general functions, make this a sort of all-in-one function: way of assembling the similar definitions together (that is applying the same arguments to different funcitons...)...
 export class UtilFunctions<StringType = string> {
 	functions: { [a: string]: Function }
-	constructor() {
+	// TODO: Write the type for the default params; then, after making the type, pray do use it here for a "defaultParams" class variable; then, use the set ones as default in the newly defined functions... The defaultParams object should be passed to the constructor....
+	constructor(defaultParams: UtilParams) {
 		// TODO: list and write all the definitions....
 		this.functions["delimited"] = () => {}
 		this.functions["recursiveIndexation"] = () => {}
+		this.functions["recursiveSetting"] = () => {}
 		this.functions["read"] = () => {}
+		this.functions["readSequence"] = () => {}
 		this.functions["readWhilst"] = this.functions["readWhile"] = () => {}
+		this.functions["whileDo"] = () => {}
 	}
 	call(funcname: string, ...args: any[]) {
-		return read(this.functions, funcname, ...args)
+		return read({
+			typestable: this.functions,
+			type: funcname,
+			args: [...args],
+		})
 	}
 }
+
+type FunctionTable = { [a: Key]: Function }
+type Key = string | number | symbol
+// TODO: finish...
+type UtilParams = {}
