864e52ff2dbdf0e56fa40ed9d3e92a5680e6a3d4
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-03-15 10:00:18 +0300

    adding the missing `regex` jsdoc

--- src/regex.ts
+++ src/regex.ts
@@ -5,12 +5,34 @@ import { string } from "@hgargg-0710/one"
 
 const { lastIndex } = string
 
-export const regex_contents = (r: RegExp) =>
-	((x) => x.slice(1, lastIndex(x) - r.flags.length))(r.toString())
+/**
+ * Returns the `string` contents of the given regular expression,
+ * such that it can be used with `RegExp` constructor to 
+ * reconstruct the regular expression. 
+ * 
+ * Note: the `/` at the sides are missing
+ */
+export const regex_contents = (r: RegExp) => {
+	const contents = r.toString()
+	return contents.slice(1, lastIndex(contents) - r.flags.length)
+}
 
-export const and = (...regexes: RegExp[]) => regex(regexes.map(non_bracket).join(""))
+/**
+ * Returns a regular expression obtained by 
+ * concatenating the given `regexes`
+*/
+export const and = (...regexes: RegExp[]) =>
+	regex(regexes.map(non_bracket).join(""))
 
-export const regex = (string: string | RegExp) => new RegExp(string)
+/**
+ * Constructs a regular expression using `from`
+ * 
+ * If a `string` is given: 
+ * 
+ * 1. the object will be created without flags
+ * 2. `from` must not contain the wrapping `/` RegExp-literal symbols
+*/
+export const regex = (from: string | RegExp) => new RegExp(from)
 
 export * as assertions from "./regex/assertions.js"
 export * as charclass from "./regex/charclass.js"
