b08c41e0dcfb0ce668ffc7ae250badf369780926
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-03-18 21:26:59 +0300

    removing redundant tests

--- tests/src/Tokenizable/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import { arraysSame, tripleUtilTest } from "lib/lib.js"
-import { matchString, tokenizeMatched } from "../../../../dist/src/Tokenizable/utils.js"
-
-export const [matchStringTest, tokenizeMatchedTest] = [
-	[matchString, "matchString"],
-	[tokenizeMatched, "tokenizeMatched"]
-].map(([util, name]) => tripleUtilTest(util as Function, name as string, arraysSame))
