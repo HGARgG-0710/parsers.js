68546b05b7b900172bad2ace32f048534b013f3d
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-03-21 22:06:13 +0300

    adding the code for `ICollection` tests

--- tests/src/Collection/lib/classes.ts
+++ tests/src/Collection/lib/classes.ts
@@ -1,102 +1,88 @@
+import assert from "assert"
+
 import type { ICollection } from "../../../../dist/src/Collection/interfaces.js"
-import {
-	ClassConstructorTest,
-	classTest,
-	FunctionalClassConctructorTest,
-	iterationTest,
-	PropertyMethodTest,
-	signatures
-} from "lib/lib.js"
 
-import { object, boolean, type } from "@hgargg-0710/one"
-const { structCheck } = object
-const { T } = boolean
-const { isFunction } = type
+import { ClassTest, MethodTest, type Interface } from "../../lib/_lib.js"
+import { isCollection } from "../../../../dist/src/Collection/utils.js"
 
-const isCollection = structCheck<ICollection>({
-	value: T,
-	push: isFunction,
-	[Symbol.iterator]: isFunction
+import { array } from "@hgargg-0710/one"
+
+const push = new MethodTest("push", function <Type = any>(
+	this: ICollection<Type>,
+	...items: Type[]
+) {
+	const prior = array.copy(this.get() as Type[])
+
+	this.push(...items)
+
+	const endIndex = array.lastIndex(prior)
+
+	for (let i = 0; i < prior.length; ++i)
+		assert.strictEqual(this.get()[i], prior[i])
+
+	for (let i = 0; i < items.length; ++i)
+		assert.strictEqual(this.get()[endIndex + i], items[i])
 })
 
-const collectionPrototypeProps = ["push", Symbol.iterator]
-const collectionOwnProps = ["value"]
-
-const CollectionConstructorTest = ClassConstructorTest<ICollection>(
-	isCollection,
-	collectionPrototypeProps,
-	collectionOwnProps
-)
-
-const CollectionFunctionalConstructorTest = FunctionalClassConctructorTest(
-	isCollection,
-	collectionPrototypeProps,
-	collectionOwnProps
-)
-
-export const CollectionPushTest =
-	PropertyMethodTest("value")<ICollection>("push")
-	
-const CollectionIterationTest = iterationTest<ICollection>
-
-export type CollectionClassTestSignature = {
-	input: any
-	pushed: any[]
-	expectedPushValue: any
-	pushCompare: (x: any, y: any) => boolean
-	iteratedOver: any[]
-	iterationCompare?: (x: any, y: any) => boolean
-}
-
-export function CollectionClassTest(
-	className: string,
-	collectionConstructor:
-		| (new (x: any) => ICollection)
-		| ((x: any) => ICollection),
-	testSignatures: CollectionClassTestSignature[],
-	functionalConstructor: boolean = false
+const get = new MethodTest("get", function <Type = any>(
+	this: ICollection<Type>,
+	expected: Iterable<Type>
 ) {
-	classTest(`(Collection) ${className}`, () =>
-		signatures(
-			testSignatures,
-			({
-					input,
-					pushed,
-					expectedPushValue,
-					pushCompare,
-					iteratedOver,
-					iterationCompare
-				}) =>
-				() => {
-					const collectionInstance = functionalConstructor
-						? CollectionFunctionalConstructorTest(
-								collectionConstructor as (
-									x: any
-								) => ICollection,
-								input
-						  )
-						: CollectionConstructorTest(
-								collectionConstructor as new (
-									x: any
-								) => ICollection,
-								input
-						  )
+	assert(array.same(this.get(), expected))
+	assert(array.same(this.get(), this.get()))
+})
 
-					// [Symbol.iterator]
-					CollectionIterationTest(
-						collectionInstance,
-						iteratedOver,
-						iterationCompare
-					)
+const SymbolIterator = new MethodTest("Symbol.iterator", function <Type = any>(
+	this: ICollection<Type>
+) {
+	assert(array.same(this.get(), this))
+})
 
-					// .push
-					CollectionPushTest(
-						collectionInstance,
-						expectedPushValue,
-						pushed,
-						pushCompare
-					)
-				}
+const copy = new MethodTest("copy", function <Type = any>(
+	this: ICollection<Type>,
+	diff: Type[]
+) {
+	const duplicate = this.copy()
+
+	assert.notStrictEqual(this, duplicate)
+	assert(array.same(this, duplicate))
+
+	this.push(...diff)
+	assert(!array.same(this, duplicate))
+})
+
+class CollectionTest<Type = any> extends ClassTest<ICollection<Type>> {
+	static readonly interfaceName = "ICollection"
+
+	static conformance(x: any) {
+		return isCollection(x)
+	}
+
+	push(...items: Type[]) {
+		return this.testMethod("push", ...items)
+	}
+
+	get(expected: Iterable<Type>) {
+		return this.testMethod("get", expected)
+	}
+
+	SymbolIterator() {
+		return this.testMethod("Symbol.iterator")
+	}
+
+	copy(...diff: Type[]) {
+		return this.testMethod("copy", ...diff)
+	}
+
+	constructor(
+		interfaces: Interface[] = [],
+		methods: MethodTest<ICollection<Type>>[] = []
+	) {
+		super(
+			[CollectionTest, ...interfaces],
+			[push, get, SymbolIterator, copy, ...methods]
 		)
-	)
+	}
 }
+
+export const CollectionTestObject = new CollectionTest()
