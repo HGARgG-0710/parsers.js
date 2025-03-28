8d9fca3e7fbd702499a89e7ea1eb5ce1dd0ad1b8
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-03-28 16:18:53 +0300

    removing the `Token` tests

--- tests/src/Token/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import { utilTest } from "lib/lib.js"
-import { isType, isToken } from "../../../../dist/src/Token/utils.js"
-
-export const [isTypeTest, isTokenTest] = [
-	[(type: any, object: any) => isType(type)(object), "isType"],
-	[isToken, "isToken"]
-].map(([util, name]) => utilTest(util as Function, name as string))
