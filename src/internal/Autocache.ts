d0622d3f00120561daaf2a6408c23447bd1c155b
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-04-08 19:12:43 +0300

    adding the `Autocache` internal class`

---
+++ src/internal/Autocache.ts
@@ -0,0 +1,22 @@
+import type { IIndexable, ISettable } from "../interfaces.js"
+import { Callable } from "./Callable.js"
+
+export class Autocache<KeyType = any, ValueType = any> extends Callable {
+	protected __call__(x: KeyType) {
+		const cached = this.value.index(x)
+		if (!cached) {
+			const newlyCached = this.callback(x)
+			this.value.set(x, newlyCached)
+			return newlyCached
+		}
+		return cached
+	}
+
+	constructor(
+		protected value: ISettable<KeyType, ValueType> &
+			IIndexable<ValueType | undefined>,
+		protected callback: (x: KeyType) => ValueType
+	) {
+		super()
+	}
+}
