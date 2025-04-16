3a119e22994e1e787579d833f507bef037c2cc9f
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-04-17 01:10:34 +0300

    restructuring the tests' modules

--- tests/src/Stream/InputStream/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { predicateUtilTest } from "lib/lib.js"
-import { utils } from "../../../../../dist/main.js"
-import { isInputStream } from "./classes.js"
-const { toInputStream } = utils.Stream.InputStream
-
-export const toInputStreamTest = predicateUtilTest(isInputStream)(
-	toInputStream,
-	"toInputStream"
-)
