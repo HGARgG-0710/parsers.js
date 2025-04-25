573601355b82ebdc218d21283ca636db081f6877
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-04-25 03:37:13 +0300

    adding the `DelegateStream`

---
+++ src/internal/DelegateStream.ts
@@ -0,0 +1,58 @@
+import type { IStream } from "../interfaces.js"
+
+export class DelegateStream<Type = any>
+	implements IStream<Type, [IStream<Type>]>
+{
+	["constructor"]: new (resource?: IStream<Type>) => typeof this
+
+	set curr(newCurr: Type) {
+		this.resource!.curr = newCurr
+	}
+
+	get curr() {
+		return this.resource!.curr
+	}
+
+	set isEnd(newEnd: boolean) {
+		this.resource!.isEnd = newEnd
+	}
+
+	get isEnd() {
+		return this.resource!.isEnd
+	}
+
+	get isStart() {
+		return this.resource!.isStart!
+	}
+
+	prev() {
+		return this.resource!.prev!()
+	}
+
+	next() {
+		return this.resource!.next()
+	}
+
+	*[Symbol.iterator]() {
+		yield* this.resource!
+	}
+
+	copy() {
+		return new this.constructor(this.resource?.copy())
+	}
+
+	isCurrStart() {
+		return this.resource!.isCurrStart!()
+	}
+
+	isCurrEnd() {
+		return this.resource!.isCurrEnd!()
+	}
+
+	init(resource: IStream<Type>) {
+		this.resource = resource
+		return this
+	}
+
+	constructor(public resource?: IStream<Type>) {}
+}
