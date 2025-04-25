7deef0ff920257399048b73da60354ac83d47c79
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-04-25 10:34:44 +0300

    adding the `StreamDigger` factory-function

--- src/classes/PropDigger.ts
+++ src/classes/PropDigger.ts
@@ -1,8 +1,15 @@
-import type { Summat } from "@hgargg-0710/summat.ts"
 import { type } from "@hgargg-0710/one"
+import type { Summat } from "@hgargg-0710/summat.ts"
 import assert from "assert"
 
 const { isStruct, isArray } = type
+
+function repeat<Type = any>(array: Type[], times: number) {
+	return Array.from(
+		{ length: array.length * times },
+		(_x, i) => array[i % array.length]
+	)
+}
 
 export class PropDigger {
 	dig<In extends object = Summat, Out extends object = any>(
@@ -15,7 +22,14 @@ export class PropDigger {
 		return currentLevel as unknown as Out
 	}
 
+	join(digger: PropDigger) {
+		return new PropDigger(this.properties.concat(digger.properties))
+	}
+
 	constructor(private readonly properties: string[]) {
 		assert(isArray(properties))
 	}
 }
+
+export const StreamDigger = (n: number) =>
+	new PropDigger(repeat(["resource"], n))
