18ca0c4a25e4a3bd120b3667cb6446b93c2e7dcc
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-04-26 17:30:11 +0300

    removing the abstract `BaseHashMap` class

--- src/HashMap/classes.ts
+++ src/HashMap/classes.ts
@@ -1,7 +1,5 @@
-import { type as _type, functional, object, string } from "@hgargg-0710/one"
+import { type as _type, functional, string } from "@hgargg-0710/one"
 import { type } from "src/Node/utils.js"
-import { OptimizedLinearMap } from "../IndexMap/LinearIndexMap/classes.js"
-import { Autocache } from "../internal/Autocache.js"
 import { length } from "../utils.js"
 import type { IHash, IHashClass, IHashMap } from "./interfaces.js"
 import type { IInternalHash } from "./InternalHash/interfaces.js"
@@ -10,74 +8,18 @@ import { extend } from "./refactor.js"
 const { id } = functional
 const { typeOf } = _type
 const { charCodeAt } = string
-const { extendPrototype } = object
-const { ConstDescriptor } = object.descriptor
 
-abstract class BaseHashClass<
+export function HashClass<
 	KeyType = any,
 	ValueType = any,
 	InternalKeyType = any,
 	DefaultType = any
-> implements IHashMap<KeyType, ValueType, InternalKeyType, DefaultType>
-{
-	["constructor"]: new (
-		value: IInternalHash<InternalKeyType, ValueType>
-	) => this
-
+>(
 	hash: IHash<KeyType, InternalKeyType>
-
-	get default() {
-		return this.internal.default
-	}
-
-	get size() {
-		return this.internal.size
-	}
-
-	index(x: KeyType, ...y: any[]) {
-		return this.internal.get(this.hash(x, ...y))
-	}
-
-	set(key: KeyType, value: ValueType, ...y: any[]) {
-		this.internal.set(this.hash(key, ...y), value)
-		return this
-	}
-
-	delete(key: KeyType, ...y: any[]) {
-		this.internal.delete(this.hash(key, ...y))
-		return this
-	}
-
-	rekey(keyFrom: KeyType, keyTo: KeyType, ...y: any[]) {
-		this.internal.rekey(this.hash(keyFrom, ...y), this.hash(keyTo, ...y))
-		return this
-	}
-
-	copy() {
-		return new this.constructor(this.internal.copy())
-	}
-
-	constructor(
-		protected internal: IInternalHash<
-			InternalKeyType,
-			ValueType,
-			DefaultType
-		>
-	) {}
-}
-
-export const HashClass = new Autocache(new OptimizedLinearMap(), function <
-	KeyType = any,
-	ValueType = any,
-	InternalKeyType = any,
-	DefaultType = any
->(hash: IHash<KeyType, InternalKeyType>) {
-	class hashClass extends BaseHashClass<
-		KeyType,
-		ValueType,
-		InternalKeyType,
-		DefaultType
-	> {
+): IHashClass<KeyType, ValueType, InternalKeyType, DefaultType> {
+	class hashClass
+		implements IHashMap<KeyType, ValueType, InternalKeyType, DefaultType>
+	{
 		static hash: IHash<KeyType, InternalKeyType> = hash
 		static extend: (
 			f: (x: any) => KeyType
@@ -85,22 +27,54 @@ export const HashClass = new Autocache(new OptimizedLinearMap(), function <
 			KeyType,
 			ValueType,
 			InternalKeyType
-		>
+		>;
+
+		["constructor"]: new (
+			value: IInternalHash<InternalKeyType, ValueType>
+		) => this
+
+		get default() {
+			return this.internal.default
+		}
+
+		get size() {
+			return this.internal.size
+		}
+
+		index(x: KeyType, ...y: any[]) {
+			return this.internal.get(hash(x, ...y))
+		}
+
+		set(key: KeyType, value: ValueType, ...y: any[]) {
+			this.internal.set(hash(key, ...y), value)
+			return this
+		}
+
+		delete(key: KeyType, ...y: any[]) {
+			this.internal.delete(hash(key, ...y))
+			return this
+		}
+
+		rekey(keyFrom: KeyType, keyTo: KeyType, ...y: any[]) {
+			this.internal.rekey(hash(keyFrom, ...y), hash(keyTo, ...y))
+			return this
+		}
+
+		copy() {
+			return new this.constructor(this.internal.copy())
+		}
+
+		constructor(
+			private internal: IInternalHash<
+				InternalKeyType,
+				ValueType,
+				DefaultType
+			>
+		) {}
 	}
 
-	extendPrototype(hashClass, {
-		hash: ConstDescriptor(hash)
-	})
-
 	return hashClass
-}) as unknown as <
-	KeyType = any,
-	ValueType = any,
-	InternalKeyType = any,
-	DefaultType = any
->(
-	hash: IHash<KeyType, InternalKeyType>
-) => IHashClass<KeyType, ValueType, InternalKeyType, DefaultType>
+}
 
 export const BasicHash = HashClass(id)
 
