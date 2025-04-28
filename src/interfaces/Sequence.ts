c51754819a7ab8b913a60b8ffffa3b3b9fdc317d
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-04-28 15:19:32 +0300

    removing the `ISequence` interface

--- src/interfaces/Sequence.ts
+++ src/interfaces/Sequence.ts
@@ -1,15 +1,11 @@
-import type { ICollection } from "../interfaces.js"
+import type { ICollection, IReadable } from "../interfaces.js"
 
-export interface ISequence<Type = any>
+export interface IDynamicSequence<Type = any>
 	extends ICollection<Type>,
-		Iterable<Type> {
-	write: (i: number, value: Type) => this
-	emptied: () => this
-	read: (i: number) => Type
-}
-
-export interface IDynamicSequence<Type = any> extends ISequence<Type> {
+		Iterable<Type>,
+		IReadable<Type> {
 	insert: (i: number, ...values: Type[]) => this
 	remove: (i: number, count?: number) => this
 	truncate: (from: number, to?: number) => this
+	emptied: () => this
 }
