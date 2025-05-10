1783a0038365613f55b3944e24a8367c25302ed6
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-05-10 20:07:09 +0300

    fixing the `IIndexStream` interface

--- src/Stream/interfaces/IndexStream.ts
+++ src/Stream/interfaces/IndexStream.ts
@@ -2,5 +2,5 @@ import type { ILineIndex } from "../interfaces.js"
 import type { IOwnedStream } from "./OwnedStream.js"
 
 export type IIndexStream<Type = any> = IOwnedStream<Type> & {
-	lineIndex: ILineIndex
+	readonly lineIndex: ILineIndex
 }
