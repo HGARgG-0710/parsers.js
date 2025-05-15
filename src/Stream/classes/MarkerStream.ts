0870c0ec82556bf4e740cc3431a35a190a78cfae
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-05-15 18:12:30 +0300

    cleaning up `MarkerStream`

--- src/Stream/classes/MarkerStream.ts
+++ src/Stream/classes/MarkerStream.ts
@@ -1,36 +1,50 @@
 import type { IOwnedStream } from "../../interfaces/Stream.js"
+import { maybeInit } from "../../utils.js"
 import type { IMarkerStream } from "../interfaces/MarkerStream.js"
 import { WrapperStream } from "./WrapperStream.js"
 
+class _MarkerStream<Type = any, MarkerType = any>
+	extends WrapperStream<Type>
+	implements IMarkerStream<Type, MarkerType>
+{
+	private marker: () => MarkerType
+
+	currMarked: MarkerType
+
+	private updateMarker() {
+		this.currMarked = this.marker()
+	}
+
+	next() {
+		const curr = super.next()
+		this.updateMarker()
+		return curr
+	}
+
+	prev() {
+		const curr = super.next()
+		this.updateMarker()
+		return curr
+	}
+
+	init(resource: IOwnedStream<Type>) {
+		super.init(resource)
+		this.updateMarker()
+		return this
+	}
+
+	setMarker(marker: () => MarkerType) {
+		this.marker = marker
+		return this
+	}
+}
+
 export function MarkerStream<Type = any, MarkerType = any>(
-	marker: (stream: IMarkerStream<Type, MarkerType>) => MarkerType
-): new (resource?: IOwnedStream<Type>) => IMarkerStream<Type, MarkerType> {
-	return class
-		extends WrapperStream<Type>
-		implements IMarkerStream<Type, MarkerType>
-	{
-		currMarked: MarkerType
-
-		private updateMarker() {
-			this.currMarked = marker(this)
-		}
-
-		next() {
-			const curr = super.next()
-			this.updateMarker()
-			return curr
-		}
-
-		prev() {
-			const curr = super.next()
-			this.updateMarker()
-			return curr
-		}
-
-		init(resource: IOwnedStream<Type>) {
-			super.init(resource)
-			this.updateMarker()
-			return this
-		}
+	marker: () => MarkerType
+) {
+	return function (
+		resource?: IOwnedStream<Type>
+	): IMarkerStream<Type, MarkerType> {
+		return maybeInit(new _MarkerStream().setMarker(marker), resource)
 	}
 }
