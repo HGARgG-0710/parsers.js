1313ac4c22575d1cde178608f4e1c3e9f58352e8
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-05-15 14:50:17 +0300

    renaming `PredicateStream` to `FilterStream`

---
+++ src/Stream/classes/FilterStream.ts
@@ -0,0 +1,57 @@
+import type {
+	ILinkedStream,
+	IOwnedStream,
+	IPredicatePosition
+} from "../../interfaces/Stream.js"
+import { maybeInit } from "../../utils.js"
+import { navigate } from "../../utils/Stream.js"
+import { DyssyncForwardStream } from "./WrapperStream.js"
+
+class _FilterStream<Type = any> extends DyssyncForwardStream<Type> {
+	private hasLookahead: boolean = false
+	private lookahead: Type
+	private predicate: IPredicatePosition<Type>
+
+	private updateCurr() {
+		this.curr = this.lookahead
+	}
+
+	private currGetter() {
+		this.updateCurr()
+		this.prod()
+	}
+
+	private prod() {
+		this.lookahead = navigate(this.resource!, this.predicate)
+		this.hasLookahead = this.resource!.isEnd
+	}
+
+	isCurrEnd(): boolean {
+		return !this.hasLookahead
+	}
+
+	next() {
+		const curr = super.next()
+		if (this.isCurrEnd()) this.endStream()
+		else this.currGetter()
+		return curr
+	}
+
+	setPredicate(predicate: IPredicatePosition<Type>) {
+		this.predicate = predicate.bind(this)
+		return this
+	}
+
+	init(resource: IOwnedStream<Type>) {
+		super.init(resource)
+		this.prod()
+		this.updateCurr()
+		return this
+	}
+}
+
+export function FilterStream<Type = any>(predicate: IPredicatePosition<Type>) {
+	return function (resource?: IOwnedStream<Type>): ILinkedStream<Type> {
+		return maybeInit(new _FilterStream().setPredicate(predicate), resource)
+	}
+}
