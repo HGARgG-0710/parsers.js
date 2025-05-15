549695ae008e412d964c358276e6b65ea12e4851
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-05-15 18:10:47 +0300

    cleaning up `IndexStream` (no more memory waste on anonymous classes)

--- src/Stream/classes/IndexStream.ts
+++ src/Stream/classes/IndexStream.ts
@@ -1,23 +1,39 @@
 import type { ILineIndex, IOwnedStream } from "../../interfaces/Stream.js"
+import { maybeInit } from "../../utils.js"
 import type { IIndexStream } from "../interfaces/IndexStream.js"
 import { LineIndex } from "./Position.js"
 import { WrapperStream } from "./WrapperStream.js"
 
-export function IndexStream<Type = any>(
-	isNewline: (x: IOwnedStream<Type>) => boolean
-): new (resource?: IOwnedStream<Type>) => IIndexStream<Type> {
-	return class extends WrapperStream<Type> implements IIndexStream<Type> {
-		next() {
-			const curr = super.next()
-			this.lineIndex[isNewline(this) ? "nextChar" : "nextLine"]()
-			return curr
-		}
+class _IndexStream<Type = any>
+	extends WrapperStream<Type>
+	implements IIndexStream<Type>
+{
+	private isNewline: () => boolean
 
-		constructor(
-			resource?: IOwnedStream<Type>,
-			public readonly lineIndex: ILineIndex = new LineIndex()
-		) {
-			super(resource)
-		}
+	next() {
+		const curr = super.next()
+		this.lineIndex[this.isNewline() ? "nextChar" : "nextLine"]()
+		return curr
+	}
+
+	setNewlinePredicate(isNewline: () => boolean) {
+		this.isNewline = isNewline
+		return this
+	}
+
+	constructor(
+		resource?: IOwnedStream<Type>,
+		public readonly lineIndex: ILineIndex = new LineIndex()
+	) {
+		super(resource)
+	}
+}
+
+export function IndexStream<Type = any>(isNewline: () => boolean) {
+	return function (resource?: IOwnedStream<Type>) {
+		return maybeInit(
+			new _IndexStream().setNewlinePredicate(isNewline),
+			resource
+		)
 	}
 }
