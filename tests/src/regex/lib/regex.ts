0b9a595a86228dfa5218756d0b1f738b8a4596ec
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-05-28 22:30:02 +0300

    removing ancient tests

--- tests/src/regex/lib/regex.ts
+++
@@ -1,8 +0,0 @@
-import { utilTest } from "lib/lib.js"
-
-import { regex as _regex } from "../../../../dist/main.js"
-import { regexTest } from "./lib.js"
-const { regex_contents, and } = _regex
-
-export const regex_contents_test = utilTest(regex_contents, "regex_contents")
-export const and_test = regexTest("and", and)
