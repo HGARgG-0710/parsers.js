184ab30244609114910265c800af239d679a5d38
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-06-01 01:33:13 +0300

    adding notes

--- tests/src/mixin/classes.ts
+++ tests/src/mixin/classes.ts
@@ -18,3 +18,16 @@ test("mixin (#1)", () => {
 		}
 	})
 })
+
+// TODO [any order]:
+// * 0 non-empty .constructor
+// * 1. [.properties] getter + setter test
+// * 2. [.properties] getter-only test
+// * 3. [.properties] setter-only test
+// * 4. [.properties] method test
+// * 5. [.properties] state variables (default props) test
+// * 6. [.properties] state-vars + methods + getter(s) + setter(s) test
+// * 7. [.properties] CONSTRUCTOR + state-vars + methods + getter(s) + setter(s) test
+// * 8. class parent + [.properties] CONSTRUCTOR + state-vars + methods + getter(s) + setter(s) test
+// * 9. mixin parent + [.properties] CONSTRUCTOR + state-vars + methods + getter(s) + setter(s) test
+// * 10. class parent + mixin parent + [.properties] CONSTRUCTOR + state-vars + methods + getter(s) + setter(s) test
