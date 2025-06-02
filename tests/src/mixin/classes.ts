7417b8d31428e92d0c85bb50b8560c4f9a059bf5
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-06-02 17:54:47 +0300

    making notes

--- tests/src/mixin/classes.ts
+++ tests/src/mixin/classes.ts
@@ -45,47 +45,30 @@ mixinTestCounter.test([HAS_CONSTRUCTOR, NO_PARENTS], () => {
 	})
 })
 
-// TODO [any order]:
-// ! VITAL: add the 'MixinInstanceTest's TOGETHER with the `MixinPureTest`-s [ALWAYS - even when there are NO ]
-// 		% reason: 
-// 			1. [when no '.constructor'] mildly "tighter" tests - eliminates certain (less elaborate) cases of 'Proxy' usage for implementation [like when '.prototype' is being substituted by a Mock of sorts]; 
-// 			2. [when IS '.consructor'] to test the behaviour of the '.constructor'
-// TODO: shorten the list of tests - to be less tautological [currently - a bit too much...]; 
-// ! -1. ADD the TESTS for `mixin`s WITHOUT constructors [MixinPureTest-s + MixinInstanceTest-s]:
-// 		* 1. with .properties:
-// 			1. getters-only
-// 			2. setters-only
-// 			3. methods-only
-// 			4. state-variables-only
-// 			5. getters + setters + methods + state-variables
-// 		* 2. with parents:
-// 			* 1. with classes:
-// 				1. getters-only
-// 				2. setters-only
-// 				3. methods-only
-// 				4. state-variables-only
-// 				5. getters + setters + methods + state-variables
-// 			* 2. with other mixins:
-// 				1. getters-only
-// 				2. setters-only
-// 				3. methods-only
-// 				4. state-variables-only
-// 				5. getters + setters + methods + state-variables
-// 				6. with classes [also]:
-// 					1. getters-only
-// 					2. setters-only
-// 					3. methods-only
-// 					4. state-variables-only
-// 					5. getters + setters + methods + state-variables
-// [MixinPrototypeTest + MixinInstanceTest]
-// * 0 non-empty .constructor [still no .properties]
-// * 1. [.properties] getter + setter test
-// * 2. [.properties] getter-only test
-// * 3. [.properties] setter-only test
-// * 4. [.properties] method test
-// * 5. [.properties] state variables (default props) test
-// * 6. [.properties] state-vars + methods + getter(s) + setter(s) test
-// * 7. [.properties] CONSTRUCTOR + state-vars + methods + getter(s) + setter(s) test
-// * 8. class parent + [.properties] CONSTRUCTOR + state-vars + methods + getter(s) + setter(s) test
-// * 9. mixin parent + [.properties] CONSTRUCTOR + state-vars + methods + getter(s) + setter(s) test
-// * 10. class parent + mixin parent + [.properties] CONSTRUCTOR + state-vars + methods + getter(s) + setter(s) test
+// TODO: about 'parents' tests: 
+// 		* 1. add 'mixins', 'classes' args onto `MixinPrototypeTest` and 'PureMixinPrototypeTest'
+// 		* 2. SAVE the 'classes' + 'mixins', SO THAT '.super' could be tested INDEPENDENTLY - REMOVE 'super : {...}' from the prototype-tests; 
+
+// TODO: tests left to do: 
+// * 1. with .constructor [MixinPrototypeTest + MixinInstanceTest]: 
+// 		1. no parents:
+// 			1. getters + setters + methods + prototype-vars
+// 		2. parents: 
+// 			1. 2 classes +  getters + setters + methods + prototype-vars
+// 			2. 2 mixins +  getters + setters + methods + prototype-vars
+// 			3. 1 class + 2 mixins +  getters + setters + methods + prototype-vars
+// * 2. without .constructor [PureMixinPrototypeTest + MixinInstanceTest]: 
+// 		1. without parents: 
+// 			1. .prototype-vars [default-state] + methods
+// 			2. .prototype-vars [default-state] only 
+// 			3. lone-getters and setters
+// 			4. getters + setters + methods + prototype-vars
+// 		2. with 'class' parents: 
+// 			1. 1 parent + getters + setters + methods + prototype-vars
+// 			2. 3 parents + getters + setters + methods + prototype-vars
+// 		3. with 'mixin' parents: 
+// 			1. 1 parent + getters + setters + methods + prototype-vars
+// 			2. 3 parents + gettesr + setters + methods + prototype-vars
+// 		4. with both 'class' and 'mixin' parents
+// 			1. 1 'class' parent + 1 'mixin' parent + getters + setters + methods + prototype-vars
+// 			2. 2 'class' parents + 3 'mixin' parents + gettesr + setters + methods + prototype-vars
