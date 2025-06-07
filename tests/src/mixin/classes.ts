d2f42be9f6ecaad86d1f92c0cf3655e7bc9d0b78
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-06-07 18:20:11 +0300

    adding test for two mixin parents of `mixin` submodule

--- tests/src/mixin/classes.ts
+++ tests/src/mixin/classes.ts
@@ -1,8 +1,10 @@
-import { object, type } from "@hgargg-0710/one"
+import { array, object, type } from "@hgargg-0710/one"
+import { mixin } from "../../../dist/main.js"
 import { TestCounter } from "../lib/lib.js"
 import {
 	CLASS_PARENTS,
 	HAS_CONSTRUCTOR,
+	MIXIN_PARENTS,
 	MixinInstanceTest,
 	MixinPrototypeTest,
 	NO_CONSTRUCTOR,
@@ -355,35 +357,139 @@ mixinTestCounter.test([HAS_CONSTRUCTOR, CLASS_PARENTS], () => {
 	})()
 })
 
+mixinTestCounter.test([HAS_CONSTRUCTOR, MIXIN_PARENTS], () => {
+	function parent1Constructor(x: number) {
+		this.S = x
+	}
+
+	const parent1Shape = {
+		name: "MixinParent1",
+		properties: {
+			t: 10,
+			s: 0,
+
+			set S(newS: number) {
+				this.s = newS
+			},
+
+			get S() {
+				return this.s
+			},
+
+			m() {
+				return 19
+			},
+
+			*[Symbol.iterator]() {
+				yield 10
+				yield 11
+			}
+		},
+
+		constructor: parent1Constructor
+	}
+
+	const parent1 = new mixin(parent1Shape)
+
+	function parent2Constructor(m: number) {
+		this.z = m
+	}
+
+	const parent2Shape = {
+		name: "MixinParent2",
+		properties: {
+			t: 5,
+			z: 11,
+
+			m() {
+				return this.z
+			}
+		},
+		constructor: parent2Constructor
+	}
+
+	const parent2 = new mixin(parent2Shape)
+
+	function constructor(x: number, m: number) {
+		this.super.MixinParent1.constructor.call(this, x)
+		this.super.MixinParent2.constructor.call(this, m)
+	}
+
+	const mixinShape = {
+		name: "Test5",
+		properties: {
+			z: 2,
+			get r() {
+				return 19
+			}
+		},
+		constructor
+	}
+
+	new MixinPrototypeTest(mixinShape, [parent1, parent2]).toClass({
+		constructor: {
+			value: constructor,
+			writable: true,
+			enumerable: false,
+			configurable: true
+		},
+		t: { value: 5, writable: true, enumerable: true, configurable: true },
+		s: { value: 0, writable: true, enumerable: true, configurable: true },
+		S: {
+			get: Object.getOwnPropertyDescriptor(parent1Shape.properties, "S")!
+				.get!,
+			set: Object.getOwnPropertyDescriptor(parent1Shape.properties, "S")!
+				.set!,
+			enumerable: true,
+			configurable: true
+		},
+		m: {
+			value: parent2Shape.properties.m,
+			writable: true,
+			enumerable: true,
+			configurable: true
+		},
+		[Symbol.iterator]: {
+			value: parent1Shape.properties[Symbol.iterator],
+			writable: true,
+			enumerable: true,
+			configurable: true
+		},
+		z: { value: 2, writable: true, enumerable: true, configurable: true },
+		r: {
+			get: Object.getOwnPropertyDescriptor(mixinShape.properties, "r")!
+				.get,
+			set: undefined,
+			enumerable: true,
+			configurable: true
+		}
+	})
+
+	new MixinInstanceTest(mixinShape, [parent1, parent2]).withInstance({
+		r: (x: any) => x.r === 19,
+		z: (x: any) => x.z === 29,
+		t: (x: any) => x.t === 5,
+		s: (x: any) => {
+			const initS = x.S === x.s
+			const initS1 = x.S === 11
+			x.S = 3
+			return initS && initS1 && x.s === x.S && x.S === 3
+		},
+		iter: (x: any) => array.same([10, 11], [...x]),
+		m: (x: any) => x.m() === 29
+	})(11, 29)
+})
+
 // ! REMINDER[1]: this is supposed to be a bunch of CLASSIC function-based JavaScript constructors, INSTEAD of TypeScript-style classes.
 // ! REMINDER[2]: STILL haven't tried out the "parent methods" on:
 // 		* 1. function-classes
 // 		* 2. 'mixin' objects
 
 // * [MixinPrototypeTest + MixinInstanceTest]
-// 2 mixins +  getters + setters + methods + prototype-vars
-// !!! ADD [Symbol.iterator] - to make sure it does work after all...
-// TODO: finish
-// mixinTestCounter.test([HAS_CONSTRUCTOR, MIXIN_PARENTS], () => {
-// 	const parent1 = new mixin({ name: "Parent1", properties: {} })
-// 	const parent2 = new mixin({ name: "Parent2", properties: {} })
-
-// 	function constructor() {}
-
-// 	const mixinShape = { name: "Test5", properties: {}, constructor }
-
-// 	new MixinPrototypeTest(mixinShape, [parent1, parent2]).toClass({})
-
-// 	const instanceTest = new MixinInstanceTest(mixinShape, [
-// 		parent1,
-// 		parent2
-// 	]).withInstance({})
-// })
-
-// * [MixinPrototypeTest + MixinInstanceTest]
 // 1 class + 2 mixins +  getters + setters + methods + prototype-vars
 // TODO: finish
 // !!! ADD [Symbol.iterator] - to make sure it does work after all...
+// !!! ADD 'this.super[methodName]' CALLS! [for both `mixin` AND `classes` - FOR ALL parents...];
 // mixinTestCounter.test([HAS_CONSTRUCTOR, CLASS_AND_MIXIN_PARENTS], () => {
 // 	class ClassParent1 {}
 
@@ -410,6 +516,7 @@ mixinTestCounter.test([HAS_CONSTRUCTOR, CLASS_PARENTS], () => {
 // * [PureMixinPrototypeTest + MixinInstanceTest]
 // 2 'class' parents + 3 'mixin' parents + gettesr + setters + methods + prototype-vars
 // TODO: finish
+// ! this is just a precautionary sanity check - no need to go all out...
 // mixinTestCounter.test([NO_CONSTRUCTOR, CLASS_AND_MIXIN_PARENTS], () => {
 // 	class ClassParent1 {}
 // 	class ClassParent2 {}
@@ -437,8 +544,6 @@ mixinTestCounter.test([HAS_CONSTRUCTOR, CLASS_PARENTS], () => {
 
 // ! TESTS are intended to verify TWO behaviours:
 // 	1. [in 'parent'-tests only...] the '.super' stuff:
-// 		1. [.constructor-tests only] presence of `super.SomeParentName.constructor`
-// 		2. ability to store MULTIPLE parents
 // 		3. ORDER of property inheritance [overriding - when a CLASS and a MIXIN, or MULTIPLE classes/mixins are added - THERE MUST be a specific property-addition order present]:
 // 			1. FIRST - copy from `class`es
 // 			2. THEN - copy from `mixin`s
