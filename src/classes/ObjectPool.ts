bd32e5ef43b13085f774038b3a794c3f126c247c
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-06-11 00:25:13 +0300

    adding the ability to do preallocations for the `ObjectPool`

--- src/classes/ObjectPool.ts
+++ src/classes/ObjectPool.ts
@@ -1,29 +1,44 @@
 import type { IInitializable } from "../interfaces.js"
+import { number } from "@hgargg-0710/one"
+
+const { max } = number
 
 class RetainedStack<Type = any> {
 	private readonly stack: Type[] = []
-	private size: number = 0
+	private usedSize: number = 0
+
+	private set maxSize(newMaxSize: number) {
+		this.stack.length = newMaxSize
+	}
 
 	private get maxSize() {
 		return this.stack.length
 	}
 
 	private isFull() {
-		return this.maxSize === this.size
+		return this.freeSpace === 0
+	}
+
+	private get freeSpace() {
+		return this.maxSize - this.usedSize
 	}
 
 	isEmpty() {
-		return this.size === 0
+		return this.usedSize === 0
 	}
 
 	pop() {
-		return this.stack[this.size--]
+		return this.stack[this.usedSize--]
 	}
 
 	push(item: Type) {
 		if (this.isFull()) this.stack.push(item)
-		else this.stack[this.size] = item
-		++this.size
+		else this.stack[this.usedSize] = item
+		++this.usedSize
+	}
+
+	ensureNew(n: number) {
+		this.maxSize += max(0, n - this.freeSpace)
 	}
 }
 
@@ -33,9 +48,18 @@ export class ObjectPool<
 > {
 	private readonly freeStack = new RetainedStack<Type>()
 
+	private allocNew(...x: Partial<TypeArgs> | []) {
+		return new this.objectConstructor(...x)
+	}
+
+	prealloc(n: number) {
+		this.freeStack.ensureNew(n)
+		for (let i = 0; i < n; ++i) this.freeStack.push(this.allocNew())
+	}
+
 	create(...x: Partial<TypeArgs>) {
 		return this.freeStack.isEmpty()
-			? new this.objectConstructor(...x)
+			? this.allocNew(...x)
 			: this.freeStack.pop().init(...x)
 	}
 
@@ -45,7 +69,7 @@ export class ObjectPool<
 
 	constructor(
 		private readonly objectConstructor: new (
-			...x: Partial<TypeArgs>
+			...x: Partial<TypeArgs> | []
 		) => Type
 	) {}
 }
