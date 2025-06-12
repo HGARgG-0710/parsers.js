1238b65d6fa09e4312dce51fd9f4f40a537c2e7a
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-06-12 22:38:06 +0300

    HashMap [interfaces]: jsdoc

--- src/interfaces/HashMap.ts
+++ src/interfaces/HashMap.ts
@@ -11,17 +11,30 @@ import type {
 } from "../interfaces.js"
 import type { IPreMap } from "../modules/HashMap/interfaces/PreMap.js"
 
+/**
+ * Represents a hash function.
+ */
 export type IHash<K = any, InternalKey = any> = (
 	x: K,
 	...y: any[]
 ) => InternalKey
 
+/**
+ * Represents an extendable factory of `IHashMap`
+ * instances based off `IPreMap` instances.
+ */
 export interface IHashClass<K = any, V = any, InternalKey = any, Default = any>
 	extends IHashable<K, InternalKey> {
 	new (structure: IPreMap<InternalKey, V, Default>): IHashMap<K, V, Default>
 	extend: (f: (x: any) => K) => IHashClass<any, V, InternalKey>
 }
 
+/**
+ * Represents an `IIndexable<V | Default>` structure with a
+ * default-value type `Default`, primary value type `V`,
+ * key type `K` and capability of being modified in a large variety of
+ * ways.
+ */
 export interface IHashMap<K = any, V = any, Default = any>
 	extends IIndexable<V | Default>,
 		ISettable<K, V | Default>,
