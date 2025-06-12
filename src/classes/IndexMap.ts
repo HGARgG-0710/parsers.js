4eef556132d12ebba414ec8ad853389abfd56c05
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-06-12 04:58:39 +0300

    refactor: IndexMap

--- src/classes/IndexMap.ts
+++ src/classes/IndexMap.ts
@@ -1,16 +1,19 @@
 import { type as _type } from "@hgargg-0710/one"
 import { is, type } from "../aliases/Node.js"
 import { curr, resource } from "../aliases/Stream.js"
+import type { IResourcefulStream, IStream, ITyped } from "../interfaces.js"
 import type { IMapClass } from "../interfaces/IndexMap.js"
 
 const { typeOf } = _type
 
-export const [typeMap, resourceMap, currMap, typeofMap] = [
-	type,
-	resource,
-	curr,
-	typeOf
-].map((x) => (mapClass: IMapClass) => mapClass.extend(x))
+export const typeMap = (mapClass: IMapClass) => mapClass.extend<ITyped>(type)
+
+export const resourceMap = (mapClass: IMapClass) =>
+	mapClass.extend<IResourcefulStream>(resource)
+
+export const currMap = (mapClass: IMapClass) => mapClass.extend<IStream>(curr)
+
+export const typeofMap = (mapClass: IMapClass) => mapClass.extend(typeOf)
 
 export const nodeMap = (mapClass: IMapClass) => mapClass.extendKey(is)
 
