4e7486f74ce7dfeb7dcf9af82c6b94f6a8dcdb09
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-06-12 21:55:59 +0300

    Accumulator: jsdoc

--- src/interfaces/Accumulator.ts
+++ src/interfaces/Accumulator.ts
@@ -8,13 +8,33 @@ import type {
 	IUnfreezable
 } from "../interfaces.js"
 
-export interface IAccumulator<T = any, Accumulated = any>
+/**
+ * This interface is intended to represent objects
+ * that are capable of being "gathered" piece-by-piece,
+ * "accumulating" to a certain value of type `T`,
+ * and encapsulated to be obtainable as `Accumulated`.
+ *
+ * Their construction can also be halted/continued at
+ * will via the `.freeze/.unfreeze()` methods.
+ */
+export interface IAccumulator<T = any, Accumulated = T>
 	extends ICopiable,
 		IFreezable,
 		IPushable<T>,
 		IGettable<Accumulated>,
 		IUnfreezable {}
 
+/**
+ * This interface is intended to serve as an
+ * extension for the `IAccumulator<T, readonly T[]>`.
+ * For objects fitting the `IPersistentAccumulator`,
+ * it is (generally) intended for the access to it
+ * via the `.read(i: number)` method to be persistent
+ * (that is, to *not* change over time, unless explicitly
+ * `.writ`-ten to).
+ *
+ * Note that it is (also) a valid `IParseable<T>`
+ */
 export interface IPersistentAccumulator<T = any>
 	extends IAccumulator<T, readonly T[]>,
 		ISizeable,
