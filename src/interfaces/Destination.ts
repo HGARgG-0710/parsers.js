c5121cfaa227ef2b384f4e313d05cdc51bf09353
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-06-12 22:20:01 +0300

    Destination: jsdoc

--- src/interfaces/Destination.ts
+++ src/interfaces/Destination.ts
@@ -1,5 +1,13 @@
 import type { ICopiable, IIsOpen, IResource } from "../interfaces.js"
 
+/**
+ * This is an interface intended to represent a
+ * holdable linearly-writable resource, which is unique, 
+ * but can be copied [a separate `.cleanup()` 
+ * call will be required]. 
+ * 
+ * The writing is performed to the end of the file. 
+ */
 export interface IDestination extends ICopiable, IResource, IIsOpen {
 	write(input: string): void
 }
