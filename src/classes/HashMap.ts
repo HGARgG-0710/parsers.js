6b547e6e1edc406e2d921a0a2ee7fdad0620a0c2
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-06-12 04:35:50 +0300

    refactor: HashMap

--- src/classes/HashMap.ts
+++ src/classes/HashMap.ts
@@ -16,55 +16,69 @@ function extend<KeyType = any, ValueType = any, InternalKeyType = any>(
 	)
 }
 
+abstract class PreHashClass<K = any, V = any, InternalKey = any, Default = any>
+	implements IHashMap<K, V, Default>
+{
+	private ["constructor"]: new (internal: IPreMap<InternalKey, V>) => this
+
+	private hash: IHash<K, InternalKey>
+
+	protected setHash(hash: IHash<K, InternalKey>) {
+		this.hash = hash
+	}
+
+	get default() {
+		return this.pre.default
+	}
+
+	get size() {
+		return this.pre.size
+	}
+
+	index(x: K, ...y: any[]) {
+		return this.pre.get(this.hash(x, ...y))
+	}
+
+	set(key: K, value: V, ...y: any[]) {
+		this.pre.set(this.hash(key, ...y), value)
+		return this
+	}
+
+	delete(key: K, ...y: any[]) {
+		this.pre.delete(this.hash(key, ...y))
+		return this
+	}
+
+	rekey(keyFrom: K, keyTo: K, ...y: any[]) {
+		this.pre.rekey(this.hash(keyFrom, ...y), this.hash(keyTo, ...y))
+		return this
+	}
+
+	copy() {
+		return new this.constructor(this.pre.copy())
+	}
+
+	concat(pairsList: Iterable<[K, V]>): this {
+		for (const [key, value] of pairsList) this.set(key, value)
+		return this
+	}
+
+	constructor(private pre: IPreMap<InternalKey, V, Default>) {}
+}
+
 export function HashClass<K = any, V = any, InternalKey = any, Default = any>(
 	hash: IHash<K, InternalKey>
 ): IHashClass<K, V, InternalKey, Default> {
-	class hashClass implements IHashMap<K, V, Default> {
+	class hashClass extends PreHashClass<K, V, InternalKey, Default> {
 		static hash: IHash<K, InternalKey> = hash
 		static extend: (f: (x: any) => K) => IHashClass<K, V, InternalKey> =
 			extend<K, V, InternalKey>
 
-		private ["constructor"]: new (internal: IPreMap<InternalKey, V>) => this
-
-		get default() {
-			return this.pre.default
+		constructor(pre: IPreMap<InternalKey, V, Default>) {
+			super(pre)
+			this.setHash(hash)
 		}
-
-		get size() {
-			return this.pre.size
-		}
-
-		index(x: K, ...y: any[]) {
-			return this.pre.get(hash(x, ...y))
-		}
-
-		set(key: K, value: V, ...y: any[]) {
-			this.pre.set(hash(key, ...y), value)
-			return this
-		}
-
-		delete(key: K, ...y: any[]) {
-			this.pre.delete(hash(key, ...y))
-			return this
-		}
-
-		rekey(keyFrom: K, keyTo: K, ...y: any[]) {
-			this.pre.rekey(hash(keyFrom, ...y), hash(keyTo, ...y))
-			return this
-		}
-
-		copy() {
-			return new this.constructor(this.pre.copy())
-		}
-
-		concat(pairsList: Iterable<[K, V]>): this {
-			for (const [key, value] of pairsList) this.set(key, value)
-			return this
-		}
-
-		constructor(private pre: IPreMap<InternalKey, V, Default>) {}
 	}
-
 	return hashClass
 }
 
