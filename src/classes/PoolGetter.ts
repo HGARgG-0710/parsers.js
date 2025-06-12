d04d114196679ec272eb96f798f9261e03cad9b7
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-06-12 09:05:41 +0300

    TypedPoolGetter: jsdoc

--- src/classes/PoolGetter.ts
+++ src/classes/PoolGetter.ts
@@ -1,6 +1,13 @@
 import type { IPoolGetter } from "../interfaces/PoolGetter.js"
 import type { ObjectPool } from "./ObjectPool.js"
 
+/**
+ * This is an object for keeping track of multiple pools, 
+ * based on some type `T`. It is mostly just a wrapper 
+ * around a `Map<T, ObjectPool>` instance. 
+ * 
+ * Implements `IPoolGetter<T>`
+*/
 export class TypedPoolKeeper<T = any> implements IPoolGetter<T> {
 	private readonly pools = new Map<T, ObjectPool>()
 
