4e0cd08431d357c00f59b5f791a28839319604c9
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-06-13 16:26:21 +0300

    Source: jsdoc

--- src/interfaces/Source.ts
+++ src/interfaces/Source.ts
@@ -1,9 +1,13 @@
-import type { ICopiable, IIsOpen, IResource } from "../interfaces.js"
+import type { ICopiable, IIsOpen, IPosed, IResource } from "../interfaces.js"
 
-export interface ISource extends ICopiable, IResource, IIsOpen {
+/**
+ * This is an interface for representing a lazily readable `IResource`,
+ * which can be `.rewind()`-ed (and, hence, reused), and the position of
+ * which can be tracked via the `readonly .pos: number`
+ */
+export interface ISource extends ICopiable, IResource, IIsOpen, IPosed<number> {
 	hasChars: () => boolean
 	nextChar: (n?: number) => void
 	rewind: () => void
 	readonly decoded: string
-	readonly pos: number
 }
