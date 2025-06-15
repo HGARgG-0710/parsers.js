bb07e5affa6ada4ab49bf79baff32768975e9d8b
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-06-15 13:23:37 +0300

    Initializer/: jsdoc

--- src/modules/Initializer/classes/ResourceInitializer.ts
+++ src/modules/Initializer/classes/ResourceInitializer.ts
@@ -3,6 +3,11 @@ import type {
 	IResourceSettable
 } from "../../../interfaces/Initializer.js"
 
+/**
+ * This is an `IInitializer` purposed to be used with `IResourceSettable`
+ * `target`s. It has a `resource` of uknown nature, and, if the `resource`
+ * is non-`null`, it is added via the `target.setResource(resource)` method call.
+ */
 export const resourceInitializer: IInitializer<[any]> = {
 	init(resourceHaving: IResourceSettable, resource?: unknown) {
 		if (resource) resourceHaving.setResource(resource)
