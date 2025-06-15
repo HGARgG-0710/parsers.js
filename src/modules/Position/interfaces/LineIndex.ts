1f1feeb8525b61d8afde45573853b1c33a54e017
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-06-15 13:41:58 +0300

    Position/: jsdoc

--- src/modules/Position/interfaces/LineIndex.ts
+++ src/modules/Position/interfaces/LineIndex.ts
@@ -1,5 +1,12 @@
 import type { ICopiable } from "../../../interfaces.js"
 
+/**
+ * This is an interface for representing a character-line
+ * pair of numbers for representation of the current position
+ * inside a file. It can be useful for various practical
+ * purposes such as diagnostics during course of input
+ * validation and error output.
+ */
 export interface ILineIndex extends ICopiable {
 	readonly char: number
 	readonly line: number
