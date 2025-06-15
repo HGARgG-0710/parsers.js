1903ae6f67c3150febda502231918f4f16a21c1b
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-06-15 21:41:58 +0300

    IndexStream: jsdoc

--- src/modules/Stream/classes/IndexStream.ts
+++ src/modules/Stream/classes/IndexStream.ts
@@ -54,6 +54,21 @@ function PreIndexStream<T = any>(): typeof IndexStreamAnnotation<T> {
 		: (indexStream = BuildIndexStream<T>() as typeof IndexStreamAnnotation)
 }
 
+/**
+ * This is a class implementing `IIndexStream<T>` and `IPrevable`. 
+ * It extends `WrapperStream<T>`. 
+ * 
+ * The stream keeps track of a `public readonly .lineIndex: ILineIndex`, 
+ * which can be used to track the "character-newline" position inside 
+ * the underlying `IOwnedStream`. 
+ * It uses the given predicate `isNewline` [which is intended to use `this`], 
+ * to analyze whether or not to consider the current element `T` of `.resource`
+ * to be a "newline" or not. In case that the predicate returns `true`, 
+ * it calls `this.lineIndex.nextLine()`, otherwise - `this.lineIndex.nextChar()`. 
+ * 
+ * The Stream is useful for error diagnostics in `IStream`-based input validators, 
+ * and/or robust parsers. 
+*/
 export function IndexStream<T = any>(isNewline: () => boolean) {
 	const indexStream = PreIndexStream<T>()
 	return function (resource?: IOwnedStream<T>) {
