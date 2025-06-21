5c0cded6ff460f6e3ca9d05f9f604d10fc5c1ab0
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-06-21 16:15:15 +0300

    ResourceCopyingStream: jsdoc

--- src/modules/Stream/classes/ResourceCopyingStream.ts
+++ src/modules/Stream/classes/ResourceCopyingStream.ts
@@ -1,6 +1,14 @@
 import type { IResourcefulStream } from "../../../interfaces.js"
 import { mixin } from "../../../mixin.js"
 
+/**
+ * This is a (sealed) mixin that requires the using party to 
+ * define the (optional) `.resource` property. It contains a `.copy` method, 
+ * which returns `new this.constructor(this.resource?.copy())`.
+ * 
+ * Intended to be used as a way to provide the "defualt" `.copy()`-method 
+ * for the classes implementing `IResourcefulStream`. 
+*/
 export const ResourceCopyingStream = new mixin.sealed<IResourcefulStream>({
 	name: "ResourceCopyingStream",
 	properties: {
