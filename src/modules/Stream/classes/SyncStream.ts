bf15704b073bfc7f0c8b756d37e028abf554ac68
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-06-21 16:32:31 +0300

    SyncStream: jsdoc

--- src/modules/Stream/classes/SyncStream.ts
+++ src/modules/Stream/classes/SyncStream.ts
@@ -1,6 +1,13 @@
+import type { IResourcefulStream } from "../../../interfaces.js"
 import { mixin } from "../../../mixin.js"
 
-export const SyncStream = new mixin.sealed({
+/**
+ * This is a (sealed) mixin that delegates the
+ * `.curr`, `.isEnd` and `.isStart` to its
+ * `.resource: IOwnedStream` [which is to be
+ * provided by the using party].
+ */
+export const SyncStream = new mixin.sealed<IResourcefulStream>({
 	name: "SyncStream",
 	properties: {
 		get curr() {
