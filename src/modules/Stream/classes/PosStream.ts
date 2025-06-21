4b6925ec9e0077c08709e7020bfbdc6b96281c05
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-06-21 16:11:53 +0300

    PosStream: jsdoc

--- src/modules/Stream/classes/PosStream.ts
+++ src/modules/Stream/classes/PosStream.ts
@@ -1,10 +1,14 @@
+import type { IPositionStream } from "../../../interfaces.js"
 import type { IPosed } from "../../../interfaces/Position.js"
 import { mixin } from "../../../mixin.js"
-import type { ILinkedStream } from "../interfaces/OwnedStream.js"
+import type { ILinkedStream, IOwnedStream } from "../interfaces/OwnedStream.js"
 import { PosHavingStream } from "./PosHavingStream.js"
 import { WrapperStream, WrapperStreamAnnotation } from "./WrapperStream.js"
 
-export class PosStreamAnnotation<T = any> extends WrapperStreamAnnotation<T> {
+export class PosStreamAnnotation<T = any>
+	extends WrapperStreamAnnotation<T>
+	implements IPositionStream<T>
+{
 	protected forward(n: number = 1) {}
 	protected backward(n: number = 1) {}
 	readonly pos: number
@@ -23,6 +27,10 @@ const PosStreamMixin = new mixin<ILinkedStream & IPosed>(
 				this.super.WrapperStream.prev.call(this)
 				this.super.PosHavingStream.prev.call(this)
 			}
+		},
+		constructor: function (resource: IOwnedStream) {
+			this.super.PosHavingStream.constructor.call(this)
+			this.super.WrapperStream.constructor.call(this, resource)
 		}
 	},
 	[],
@@ -33,6 +41,18 @@ function PrePosStream<T = any>() {
 	return PosStreamMixin.toClass() as typeof PosStreamAnnotation<T>
 }
 
+/**
+ * This is a mixin that combines:
+ *
+ * 1. `WrapperStream`
+ * 2. `PosHavingStream`
+ *
+ * For its `.next()` and `.prev()` operations, it updates the underlying
+ * `.resource: IOwnedStream`'s `.curr`, while also updating its `.pos`
+ * correspondently.
+ *
+ * It calls both the constructors from `PosHavingStream` and `WrapperStream`
+ */
 export const PosStream: ReturnType<typeof PrePosStream> & {
 	generic?: typeof PrePosStream
 } = PrePosStream()
