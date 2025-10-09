4f9eb6de42efd55c004690699ee84862956edc53
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-10-09 20:24:42 +0300

    adding a new class - `Counter`

---
+++ src/objects/Counter.ts
@@ -0,0 +1,20 @@
+/**
+ * This is a class for representing a counter.
+ * It can have an arbitrary start-point (by default, begins counting at `0`).
+ * Perfect for creation of simple unique identifiers.
+ */
+export class Counter {
+	private count: number
+
+	inc() {
+		++this.count
+	}
+
+	get() {
+		return this.count
+	}
+
+	constructor(start: number = 0) {
+		this.count = start
+	}
+}
