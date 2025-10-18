3d458b28c451f258201230c412c6ef10ab917b2d
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-10-18 15:51:49 +0300

    fixing types

--- src/modules/Stream/objects/CustomLinkedStream.d.ts
+++ src/modules/Stream/objects/CustomLinkedStream.d.ts
@@ -1,24 +1,16 @@
 import type { ObjectPool } from "../../../objects.ts"
-import type {
-	ILinkedStream,
-	IOwnedStream,
-	IOwningStream
-} from "../interfaces/OwnedStream.ts"
+import type { ILinkedStream, IOwningStream } from "../interfaces/OwnedStream.ts"
 import type { OwningStream } from "./OwningStream.ts"
 
-export declare abstract class CustomLinkedStream<T = any, Args extends any[] = []>
+export declare abstract class CustomLinkedStream<
+		T = any,
+		Args extends any[] = []
+	>
 	extends OwningStream<T, Args>
 	implements ILinkedStream<T>
 {
 	setOwner(newOwner: IOwningStream): void
 	readonly owner?: IOwningStream
 	free(): void
-
 	protected abstract readonly pool: ObjectPool<ILinkedStream<T>>
-
-	abstract setResource(resource: IOwnedStream): void
-	abstract next(): void
-	abstract isCurrEnd(): boolean
-	abstract readonly curr: T
-	abstract readonly isEnd: boolean
 }
