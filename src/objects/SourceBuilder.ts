9c724e9030671084916c2c930210d63b2a5ed539
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2025-12-02 03:23:59 +0300

    refactor: `SourceBuilder` via the State and Proxy design patterns

--- src/objects/SourceBuilder.ts
+++ src/objects/SourceBuilder.ts
@@ -1,5 +1,6 @@
 import { type } from "@hgargg-0710/one"
 import assert from "assert"
+import type { IPrototypeCollection } from "../interfaces.js"
 import type { IAccumulator } from "../interfaces/Accumulator.js"
 
 const { isString } = type
@@ -8,49 +9,87 @@ const { isString } = type
  * This is a class for a piece-wise construction
  * of a string primitive. Implements `IAccumulator<string>`.
  */
-export class SourceBuilder implements IAccumulator<string> {
+export class SourceBuilder
+	implements IAccumulator<string>, IPrototypeCollection<string, string>
+{
 	private ["constructor"]: new (finalSource?: string) => this
 
-	private _isFrozen: boolean = false
-
-	private set isFrozen(newIsFrozen: boolean) {
-		this._isFrozen = newIsFrozen
-	}
+	private readonly proxy: StringProxy
+	private readonly frozen: SourceBuilderFrozen
+	private readonly unfrozen: SourceBuilderUnfrozen
+	private state: ISourceBuilderState
 
 	get isFrozen() {
-		return this._isFrozen
+		return this.state === this.frozen
 	}
 
 	copy() {
-		const copy = new this.constructor(this.finalSource)
+		const copy = new this.constructor(this.source)
 		if (this.isFrozen) copy.freeze()
 		return copy
 	}
 
 	unfreeze() {
-		this.isFrozen = false
+		this.state = this.unfrozen
 		return this
 	}
 
 	freeze() {
-		this.isFrozen = true
+		this.state = this.frozen
 		return this
 	}
 
 	get() {
-		return this.finalSource
+		return this.proxy.retrieve()
 	}
 
 	push(...strings: string[]) {
-		if (!this.isFrozen) this.finalSource += strings.join("")
+		this.state.push(...strings)
 		return this
 	}
 
 	clear() {
-		this.finalSource = ""
+		this.proxy.clear()
+		this.unfreeze()
 	}
 
-	constructor(private finalSource: string = "") {
-		assert(isString(finalSource))
+	constructor(private source: string = "") {
+		assert(isString(source))
+		this.proxy = new StringProxy(source)
+		this.frozen = new SourceBuilderFrozen()
+		this.unfrozen = new SourceBuilderUnfrozen(this.proxy)
+		this.state = this.unfrozen
 	}
+}
+
+interface ISourceBuilderState {
+	push(...strings: string[]): void
+}
+
+class StringProxy {
+	append(strings: string[]) {
+		this.source += strings.join("")
+	}
+
+	clear() {
+		this.source = ""
+	}
+
+	retrieve() {
+		return this.source
+	}
+
+	constructor(private source: string) {}
+}
+
+class SourceBuilderFrozen implements ISourceBuilderState {
+	push(...strings: string[]): void {}
+}
+
+class SourceBuilderUnfrozen implements ISourceBuilderState {
+	push(...strings: string[]) {
+		this.source.append(strings)
+	}
+
+	constructor(private source: StringProxy) {}
 }
